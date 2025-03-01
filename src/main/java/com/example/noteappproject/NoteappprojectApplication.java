58b862bb53519ff79f8e72ab5d1b6a637d63e0a8
Sergey Ivanov <sergeykonstwork@gmail.com>
2025-03-01 15:05:56 +0300

    Инициализация проекта

---
+++ src/main/java/com/example/noteappproject/NoteappprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteappprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteappprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/noteappproject/NoteappprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noteappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteappprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
